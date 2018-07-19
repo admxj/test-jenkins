d517428e4e9de7e66568240a2aeb459daa7715da
项金 <admxj@126.com>
2018-07-19 09:43:04 +0800

    interceptor

--- src/main/java/com/admxj/spring/SpringDemo/config/WebMvcConfigurer.java
+++ src/main/java/com/admxj/spring/SpringDemo/config/WebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.admxj.spring.SpringDemo.config;
 
 import com.admxj.spring.SpringDemo.interceptor.OneInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
+    @Autowired
+    private OneInterceptor oneInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
-        registry.addInterceptor(new OneInterceptor()).addPathPatterns("/one/**");
+        registry.addInterceptor(oneInterceptor).addPathPatterns("/one/**");
         super.addInterceptors(registry);
     }
 }
--- src/main/java/com/admxj/spring/SpringDemo/interceptor/OneInterceptor.java
+++ src/main/java/com/admxj/spring/SpringDemo/interceptor/OneInterceptor.java
@@ -4,6 +4,8 @@ import com.admxj.spring.SpringDemo.dto.SimpleJsonResult;
 import com.admxj.spring.SpringDemo.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,26 +15,32 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 
+@Component
 public class OneInterceptor implements HandlerInterceptor {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         logger.info("放行");
+
+        logger.info(((HandlerMethod)handler).getBean().getClass().getName());
+        logger.info(((HandlerMethod)handler).getMethod().getName());
 
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        logger.info("postHandle");
 
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("afterCompletion");
     }
 
     public void returnErrorResponse(HttpServletRequest request, HttpServletResponse response, SimpleJsonResult result)
